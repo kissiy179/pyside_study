cfb143fd03a89622189d6aa925abf4fda8156165
kishimoto_satoshi2 <Satoshi.Kishimoto@sega.com>
2016-12-06 16:49:37 +0900

    modelインデックスのサンプルをちゃんと動くように改修

--- model_view/modelindex.py
+++ model_view/modelindex.py
@@ -5,11 +5,26 @@ from PySide import QtCore, QtGui
 
 
 
-## pysideでは使用するタイミングですべてアイテムが読み終わってないとrowCountが取得できない。
-#  なのでとりあえず放置。
 if __name__ == '__main__':
-	app = QtGui.QApplication(sys.argv)
-	crr_dir = os.path.dirname(__file__)
-	model = QtGui.QFileSystemdModel()
-	model.setRootPath(crr_dir)
-	sys.exit(app.exec_())
+    app = QtGui.QApplication(sys.argv)
+    crr_dir = os.path.dirname(__file__)
+    model = QtGui.QFileSystemModel()
+    model.setRootPath(crr_dir)
+
+    # ↓↓↓ これやってあげないとなぜかファイルシステムのパースしない ===================================================
+    treeview = QtGui.QTreeView()
+    app.processEvents(QtCore.QEventLoop.AllEvents)
+    # ↑↑↑ ========================================================================================
+
+    parent_index = model.index(crr_dir)
+    row_count = model.rowCount(parent_index)
+    wgt = QtGui.QWidget()
+    layout = QtGui.QVBoxLayout()
+    for row in range(row_count):
+        index = model.index(row, 0, parent_index)
+        txt = index.data(QtCore.Qt.DisplayRole)
+        btn = QtGui.QPushButton(txt)
+        layout.addWidget(btn)
+    wgt.setLayout(layout)
+    wgt.show()
+    sys.exit(app.exec_())
