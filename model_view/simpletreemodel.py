02ba20e91aa2c09e1af4365d8794e0daba9fa97a
kissiy179 <kissiy179@gmail.com>
2017-02-07 03:20:25 +0900

    simpletreemodel.SimpleTreeModelをQAbstractItemModelベースに変更

--- model_view/simpletreemodel.py
+++ model_view/simpletreemodel.py
@@ -4,7 +4,7 @@ import os
 from PySide import QtCore, QtGui
 
 
-class SimpleTreeModel(QtCore.QAbstractListModel):
+class SimpleTreeModel(QtCore.QAbstractItemModel):
 	'''
 	シンプルなツリー構造を扱うモデル
 	仕様：
@@ -20,10 +20,11 @@ class SimpleTreeModel(QtCore.QAbstractListModel):
 	・ナビゲーションとインデックス生成(必須)
 	・ドラッグアンドドロップサポート
 
-	このバージョンではアイテムデータハンドリングに必要な
-	data, rowCount, columnCount, (headerData)
-	を実装し、これだけで動作するQAbstractListModelを継承している
-	次バージョンでQAbstractItemModelに置き換える
+	このバージョンではアイテムデータハンドリングに必要な関数に加え、
+	ナビゲーションとインデックス生成に必要な関数
+	index, parent
+	を実装する
+	それに伴いスーパークラスをQAbstractItemModeに変更する
 	'''
 
 	columns = ['Widget Hierarchy']  # カラムリスト
@@ -31,6 +32,7 @@ class SimpleTreeModel(QtCore.QAbstractListModel):
 	def __init__(self, parent=None):
 		'''初期化処理'''
 		super(SimpleTreeModel, self).__init__(parent)
+		self.topWidget = QtGui.QWidget()
 
 
 
@@ -76,6 +78,30 @@ class SimpleTreeModel(QtCore.QAbstractListModel):
 		'''
 		return len(self.columns)
 
+
+
+	def index(self, row, column, parent):
+		if not parent.isValid():
+			if(row == 0 and column == 0):
+				return self.createIndex(0,0, self.topWidget)
+			return QtCore.QModelIndex()
+		if column != 0 or parent.column() != 0:
+			return QtCore.QModelIndex()
+		wgt = QtGui.QWidget(parent.internalPointer())
+		children = wgt.findChildren(QtGui.QWidget)
+		return createIndex(row, 0, children[row]) if row < len(children) else QtCore.QModelIndex()
+
+
+
+	def parent(self, index):
+		if index.isValid():
+			wgt = QtGui.QWidget(index.internalPointer())
+			if not wgt == self.topWidget:
+				parent = wgt.parentWidget()
+				row = parent.findChildren(QtGui.QWidget).index(wgt)
+				if row > -1:
+					return self.createIndex(row, 0, parent)
+		return QtCore.QModelIndex()
 
 
 if __name__ == '__main__':
